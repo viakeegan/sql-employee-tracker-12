b70bdffe3cc5c01d4e4e8afcca2f1815fbc3ac1c
unknown <viakeegan@gmail.com>
2022-01-17 23:19:14 -0500

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,7 +5,7 @@ VALUES
  ("Finance"),
  ("Legal");
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES 
 ("Sales Lead", 100000, 1), 
 ("Salesperson", 80000, 1), 
@@ -15,7 +15,7 @@ VALUES
 ("Legal Team Lead", 250000, 4), 
 ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id)
+INSERT INTO employees (first_name, last_name, role_id)
 VALUES 
 ("Oscar", "Wilde", 1), 
 ("James", "Joyce", 2), 
