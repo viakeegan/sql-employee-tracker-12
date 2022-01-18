aed8a1a1bca978a897c5907ce285a6e0d26f5aba
unknown <viakeegan@gmail.com>
2022-01-17 23:17:47 -0500

    seeds add roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,17 @@
 INSERT INTO departments (name)
-  VALUES
-    ("Sales"),
-    ("Engineering"),
-    ("Finance"),
-    ("Legal");
+VALUES
+ ("Sales"),
+ ("Engineering"),
+ ("Finance"),
+ ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("Sales Lead", 100000, 1), 
+("Salesperson", 80000, 1), 
+("Lead Engineer", 150000, 2), 
+("Software Engineer", 120000, 2), 
+("Accountant", 125000, 3), 
+("Legal Team Lead", 250000, 4), 
+("Lawyer", 190000, 4);
 
