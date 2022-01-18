66c94f7eb1b740aa05ce8bfd5b1202f9d92bb7af
unknown <viakeegan@gmail.com>
2022-01-17 23:22:24 -0500

    create roles tables

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE departments (
   name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
